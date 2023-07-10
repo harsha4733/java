c5ac9e88ad73d50b19e1bf7459a6f08f677ea597
Harshavardhan Guggilla <113342518+harsha4733@users.noreply.github.com>
2023-07-10 09:01:33 +0530

    Add files via upload

---
+++ fib.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class fib
+{
+public static void main(String[]arg)
+{
+Scanner ab=new Scanner(System.in);
+int n=ab.nextInt();
+System.out.println("the fib series:");
+int a=0,b=1,c,i;
+for(i=0;i<=n;i++)
+{
+
+System.out.println(a);
+c=a+b;
+a=b;
+b=c;
+}
+}
+}
