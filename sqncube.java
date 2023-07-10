85667e8c9cde28cda4aa747e0872abc4ccb1461d
Harshavardhan Guggilla <113342518+harsha4733@users.noreply.github.com>
2023-07-10 10:07:54 +0530

    Create sqncube.java

---
+++ sqncube.java
@@ -0,0 +1,11 @@
+package harsha;
+import java.util.*;
+public class sqncube {
+	public static void main(String[]args) {
+		Scanner a=new Scanner(System.in);
+		float n=a.nextFloat();
+		System.out.println("squre:"+(n*n));
+		System.out.println("cube:"+(n*n*n));
+	}
+
+}
